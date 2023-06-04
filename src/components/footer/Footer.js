33022a52c47f597b285400e7c895f3d5bd164908
Adriiduran <adriiiduraan@gmail.com>
2023-06-04 04:05:23 +0200

    change footer color

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,7 +5,7 @@ const date = new Date();
 const year = date.getFullYear();
 
 const Footer = () => {
-  return <div className={styles.footer}>&copy; {year} All Rights Reserved</div>;
+  return <div className={styles.footer}>&copy; OnionStickers {year} - Todos los derechos reservados</div>;
 };
 
 export default Footer;
