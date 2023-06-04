e38f090431cc1f2ec7d0b210526db98d832763f8
Adriiduran <adriiiduraan@gmail.com>
2023-06-04 04:12:55 +0200

    change footer text

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,7 +5,7 @@ const date = new Date();
 const year = date.getFullYear();
 
 const Footer = () => {
-  return <div className={styles.footer}>&copy; OnionStickers {year} - Todos los derechos reservados</div>;
+  return <div className={styles.footer}>&copy; OnionStickers {year}</div>;
 };
 
 export default Footer;
